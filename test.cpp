fbdff9dedb333ee777634477509dc7d445a00d81
Julien <pgyygp090@gmail.com>
2025-01-25 21:47:34 +0100

    updated example

--- test.cpp
+++ test.cpp
@@ -1,26 +1,16 @@
 #include "layer2.h"
 
 int main() {
-  unsigned int k = 5;
-  unsigned int n = 17;
-  std::deque<bool> out = int_to_binarydq(126);
-  for (int i = 0; i < out.size(); ++i) {
-    std::cout << out[i];
-  };
-  std::cout << "\n";
-  
-  std::string teste = "bonj";
-  std::vector<unsigned int> outv = cipherer_minos(teste);
-  unsigned int out2 = outv[2];
-  std::cout << out2 << "\n";
 
-  k = outv[0];
-  n = outv[1];
+  std::string input_wrd = "word";
+  std::vector<unsigned int> vec = cipherer_minos(input_wrd);
+  unsigned int k = vec[0];
+  unsigned int n = vec[1];
+  unsigned int ciphered_wrd = vec[2];
+  std::string out_wrd = decipherer_minos(k, n, ciphered_wrd);
+  std::cout << out_wrd << "\n";
 
-  std::cout << "n: " << n << "\n";
-  std::cout << "k: " << k << "\n";
-
-  teste = decipherer_minos(k, n, out2);
-  std::cout << teste << "\n";
   return 0;
 };
+
+
